4400264bc01764da5ab9823cae928864e2da7078
Joseph Guenther <joseph@codingcool.ca>
2023-03-27 17:19:27 -0400

    Add support for windows service

--- WorkerServiceVB/Program.vb
+++ WorkerServiceVB/Program.vb
@@ -1,19 +1,18 @@
 Imports System.IO
-Imports System.Reflection
 Imports Microsoft.Extensions.DependencyInjection
 Imports Microsoft.Extensions.Hosting
 Imports Serilog
 
 Public Module Program
     Public Sub Main(args As String())
-        Environment.CurrentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
-        Log.Logger = New LoggerConfiguration().WriteTo.File("service.log").CreateLogger()
+        Dim logFile As String = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "WorkerServiceVB", "log.txt")
+        Log.Logger = New LoggerConfiguration().WriteTo.File(logFile).CreateLogger()
         Dim host As IHost = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args).
             ConfigureServices(
                 Sub(services As IServiceCollection)
                     services.AddHostedService(Of Worker)
                     services.AddSingleton(Of ActionService)
-                End Sub).Build()
+                End Sub).UseWindowsService.Build()
         host.Run()
     End Sub
 End Module
